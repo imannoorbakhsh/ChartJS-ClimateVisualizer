ddf361bf534553f5cb96be399efe2c234ceb49ad
imibeetle <imibeetle@gmail.com>
2024-01-19 12:48:51 +0330

    "Add: Create initial layout and structure"

---
+++ script.js
@@ -0,0 +1,22 @@
+// Waiting for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Sample Data',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+
+        // Configuration options
+        options: {}
+    });
+});
